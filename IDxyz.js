bbf91bccf7ec580ae590abd7b790ea30d966c243
TimT-code <ttesner+twitter@gmail.com>
2016-08-11 18:19:07 -0400

    Create IDxyz.js

---
+++ IDxyz.js
@@ -0,0 +1,18 @@
+var
+   IDx=function(id){return document.getElementById(id)},
+   IDz=function(id){return document.getElementById(id).style},
+   IDz_dno=function(id){return document.getElementById(id).style.display='none'},
+   IDz_din=function(id){return document.getElementById(id).style.display='inline'},
+   IDz_dib=function(id){return document.getElementById(id).style.display='inline-block'},
+   IDz_dbl=function(id){return document.getElementById(id).style.display='block'},
+   IDz_b=function(id,valu){return document.getElementById(id).style.border=valu},
+   IDz_bt=function(id,valu){return document.getElementById(id).style.borderTop=valu},
+   IDz_br=function(id,valu){return document.getElementById(id).style.borderRight=valu},
+   IDz_bb=function(id,valu){return document.getElementById(id).style.borderBottom=valu},
+   IDz_bl=function(id,valu){return document.getElementById(id).style.borderLeft=valu},
+   IDz_w=function(id,valu){return document.getElementById(id).style.width=valu+'px'},
+   IDz_h=function(id,valu){return document.getElementById(id).style.height=valu+'px'},
+   IDz_pt=function(id,valu){return document.getElementById(id).style.paddingTop=valu+'px'},
+   IDz_pl=function(id,valu){return document.getElementById(id).style.paddingLeft=valu+'px'},
+   ltb_8='8px solid lightblue',
+   blk_2='2px solid black';
