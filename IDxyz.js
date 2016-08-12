c48e9b8996d330d8b6710155729677bb991d62f3
TimT-code <ttesner+twitter@gmail.com>
2016-08-12 14:42:04 -0400

    shorten code
    replaced all occurences of document.getElementById(id).style
    
    with IDz(id)

--- IDxyz.js
+++ IDxyz.js
@@ -1,26 +1,21 @@
-   //(function IDxyz(window){
+   //(function IDxyz(){
    var
       IDx=function(id){return document.getElementById(id)},
-      //IDz=function(id){return document.getElementById(id).style},
       IDz=function(id){return IDx(id).style},
-      //IDz_dno=function(id){return document.getElementById(id).style.display='none'},
       IDz_dno=function(id){return IDz(id).display='none'},
-      //IDz_dnob=function(id){return IDz.display='none'},
-      IDz_din=function(id){return document.getElementById(id).style.display='inline'},
-      IDz_dib=function(id){return document.getElementById(id).style.display='inline-block'},
-      IDz_dbl=function(id){return document.getElementById(id).style.display='block'},
-      IDz_b=function(id,valu){return document.getElementById(id).style.border=valu},
-      IDz_bt=function(id,valu){return document.getElementById(id).style.borderTop=valu},
-      IDz_br=function(id,valu){return document.getElementById(id).style.borderRight=valu},
-      IDz_bb=function(id,valu){return document.getElementById(id).style.borderBottom=valu},
-      IDz_bl=function(id,valu){return document.getElementById(id).style.borderLeft=valu},
-      IDz_w=function(id,valu){return document.getElementById(id).style.width=valu+'px'},
-      IDz_h=function(id,valu){return document.getElementById(id).style.height=valu+'px'},
-      IDz_pt=function(id,valu){return document.getElementById(id).style.paddingTop=valu+'px'},
-      IDz_pl=function(id,valu){return document.getElementById(id).style.paddingLeft=valu+'px'},
+      IDz_din=function(id){return IDz(id).display='inline'},
+      IDz_dib=function(id){return IDz(id).display='inline-block'},
+      IDz_dbl=function(id){return IDz(id).display='block'},
+      IDz_b=function(id,valu){return IDz(id).border=valu},
+      IDz_bt=function(id,valu){return IDz(id).borderTop=valu},
+      IDz_br=function(id,valu){return IDz(id).borderRight=valu},
+      IDz_bb=function(id,valu){return IDz(id).borderBottom=valu},
+      IDz_bl=function(id,valu){return IDz(id).borderLeft=valu},
+      IDz_w=function(id,valu){return IDz(id).style.width=valu+'px'},
+      IDz_h=function(id,valu){return IDz(id).height=valu+'px'},
+      IDz_pt=function(id,valu){return IDz(id).style.paddingTop=valu+'px'},
+      IDz_pl=function(id,valu){return IDz(id).paddingLeft=valu+'px'},
       ltb_8='8px solid lightblue',
       blk_2='2px solid black';
       
-      var tester='12345abcdef';
-      
-   //}(window));
+   //}());
