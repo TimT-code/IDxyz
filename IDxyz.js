35c26459bde2b08c2373f90ff3e1fb694818a64c
TimT-code <ttesner+twitter@gmail.com>
2016-08-12 09:21:53 -0400

    Update IDxyz.js

--- IDxyz.js
+++ IDxyz.js
@@ -1,4 +1,4 @@
-(function IDxyz(){
+//(function IDxyz(){
    var
       IDx=function(id){return document.getElementById(id)},
       IDz=function(id){return document.getElementById(id).style},
@@ -19,4 +19,4 @@
       blk_2='2px solid black';
       
       var tester='12345abcdef';
-}(window));
+//}(window));
